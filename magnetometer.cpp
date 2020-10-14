7274ab561218841be9f98ef7e021c2a8698158bf
997volt <38107455+997volt@users.noreply.github.com>
2020-10-14 18:34:53 +0200

    add azimuth calculations

--- magnetometer.cpp
+++ magnetometer.cpp
@@ -16,31 +16,45 @@ Magnetometer::~Magnetometer()
 /// button initializing all calculations
 void Magnetometer::on_calculate_pushButton_clicked()
 {
-    QVector<float> xyzt_values = calculate_actual_values(get_xyzt_values(
-        ui->x28_input_lineEdit->text().toLongLong(), ui->x29_input_lineEdit->text().toLongLong()));
+    QVector<long long> input = get_ui_input();
+    if(check_input(input))
+    {
+        QVector<float> xyzt_values = convert_units(get_xyzt_values(input[0], input[1]));
+        float azimuth = calculate_azimuth(xyzt_values[0], xyzt_values[1]);
+        update_ui_data(xyzt_values, azimuth);
+    }
+}
+
+QVector<long long> Magnetometer::get_ui_input()
+{
+    QVector<long long> ui_input(2,0);
+
+    ui_input[0] = ui->x28_input_lineEdit->text().toLongLong();
+    ui_input[1] = ui->x29_input_lineEdit->text().toLongLong();
+
+    return ui_input;
+}
+
+void Magnetometer::update_ui_data(QVector<float> xyzt_values, float azimuth)
+{
     ui->x_lineEdit->setText(QString::number(xyzt_values[0]));
     ui->y_lineEdit->setText(QString::number(xyzt_values[1]));
     ui->z_lineEdit->setText(QString::number(xyzt_values[2]));
     ui->temp_lineEdit->setText(QString::number(xyzt_values[3]));
+    ui->azimuth_lineEdit->setText(QString::number(azimuth));
 }
 
-/// calculate XYZ magnetic field strength and IC temperature based on 0x28 and 0x29 registry values
-/// returns cector of ints (0-Xaxis, 1-Yaxis, 2-Zaxis, 3-temperature)
-QVector<int> Magnetometer::get_xyzt_values(long long x28_reg, long long x29_reg)
+bool Magnetometer::check_input(QVector<long long> input)
 {
-    QVector<int> xyzt_values(4, 0);
-    if(x28_reg < 8589934592 && x29_reg < 8589934592 && x28_reg >= 0 && x29_reg >= 0)
+    if(input[0] < 4294967296 && input[1] < 4294967296 && input[0] >= 0 && input[1] >= 0)
+        return true;
+    else
     {
-        QVector<int> x28_reg_bin = decimal_to_binary(x28_reg), x29_reg_bin = decimal_to_binary(x29_reg);
-        QVector<QVector<int>> xyzt_bin = get_xyzt_bin(x28_reg_bin, x29_reg_bin);
-
-        for (int i = 0; i < 4 ; i++)
-        {
-            xyzt_values[i] = binary_to_decimal(xyzt_bin[i]);
-        }
+        QMessageBox msgBox;
+        msgBox.setText("Input out of range. Must be a positive integer, maximally 4294967295");
+        msgBox.exec();
+        return false;
     }
-
-    return xyzt_values;
 }
 
 /// simple conversion - index 0 for LSB in all binary vectors
@@ -81,6 +95,24 @@ int Magnetometer::binary_to_decimal(QVector<int> binary)
     return decimal;
 }
 
+
+/// calculate XYZ magnetic field strength and IC temperature based on 0x28 and 0x29 registry values
+/// returns cector of ints (0-Xaxis, 1-Yaxis, 2-Zaxis, 3-temperature)
+QVector<int> Magnetometer::get_xyzt_values(long long x28_reg, long long x29_reg)
+{
+    QVector<int> xyzt_values(4, 0);
+
+    QVector<int> x28_reg_bin = decimal_to_binary(x28_reg), x29_reg_bin = decimal_to_binary(x29_reg);
+    QVector<QVector<int>> xyzt_bin = get_xyzt_bin(x28_reg_bin, x29_reg_bin);
+
+    for (int i = 0; i < 4 ; i++)
+    {
+        xyzt_values[i] = binary_to_decimal(xyzt_bin[i]);
+    }
+
+    return xyzt_values;
+}
+
 /// translate binary 0x28 and 0x29 values to binary XYZ and temperature
 QVector<QVector<int>> Magnetometer::get_xyzt_bin(QVector<int> x28_reg_bin, QVector<int> x29_reg_bin)
 {
@@ -109,7 +141,7 @@ QVector<QVector<int>> Magnetometer::get_xyzt_bin(QVector<int> x28_reg_bin, QVect
 }
 
 /// converts magnetic field strength to Gauss and temperature to Celcius
-QVector<float> Magnetometer::calculate_actual_values(QVector<int> xyzt_from_reg)
+QVector<float> Magnetometer::convert_units(QVector<int> xyzt_from_reg)
 {
     QVector<float> actual_xyzt_values(4, 0);
 
@@ -120,6 +152,39 @@ QVector<float> Magnetometer::calculate_actual_values(QVector<int> xyzt_from_reg)
     actual_xyzt_values[3] = 302 * ((float)(xyzt_from_reg[3]) - 1708) / 4096;
 
     return actual_xyzt_values;
+}
+
+
+/// calculating azimuth - angle between device orientation and magnetic north
+/// magnetometer results point at magneic north
+/// phone points at x axis, y axis is to right, z axis is cofing from the screen
+/// returns value in degrees
+float Magnetometer::calculate_azimuth(float x, float y)
+{
+    // x > 0, y > 0, az =  360 - atan(y/x) * 180 / PI;
+    // x > 0, y < 0, az =  -atan(y/x) * 180 / PI;
+    // x < 0, y > 0, az =  180 - atan(y/x) * 180 / PI;
+    // x > 0, y < 0, az =  180 + atan(y/x) * 180 / PI;
+
+    if(y == 0 && x == 0)
+        return -1;
+    else if (y == 0 && x > 0)
+        return 0;
+    else if (x == 0)
+        return 180 + 90 * sign(y);
+    else if (x > 0)
+        return 180 + 180 * sign(y) - atan(y/x) * 180 / PI;
+    else if (x < 0)
+        return 180 + sign(y) * atan(y/x) * 180 / PI;
+
+    return -1;
+}
+
+int Magnetometer::sign(float x)
+{
+    if (x > 0) return 1;
+    if (x < 0) return -1;
+    return 0;
 }
 
 
--- magnetometer.h
+++ magnetometer.h
@@ -1,9 +1,11 @@
 #ifndef MAGNETOMETER_H
 #define MAGNETOMETER_H
 
+#define PI 3.14159265
+
 #include <QMainWindow>
-#include <QVector2D>
 #include <math.h>
+#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Magnetometer; }
@@ -18,12 +20,22 @@ public:
     ~Magnetometer();
 
 private slots:
+
     void on_calculate_pushButton_clicked();
-    QVector<int> get_xyzt_values(long long x28_reg, long long x29_reg);
+    QVector<long long> get_ui_input();
+    void update_ui_data(QVector<float> xyzt_values, float azimuth);
+    bool check_input(QVector<long long> input);
+
     QVector<int> decimal_to_binary(long long decimal);
     int binary_to_decimal(QVector<int> binary);
+
+    QVector<int> get_xyzt_values(long long x28_reg, long long x29_reg);
     QVector<QVector<int>> get_xyzt_bin(QVector<int> x28_reg_bin, QVector<int> x29_reg_bin);
-    QVector<float> calculate_actual_values(QVector<int> xyzt_from_reg);
+    QVector<float> convert_units(QVector<int> xyzt_from_reg);
+
+    float calculate_azimuth(float x, float y);
+    int sign(float x);
+
 
 private:
     Ui::Magnetometer *ui;
